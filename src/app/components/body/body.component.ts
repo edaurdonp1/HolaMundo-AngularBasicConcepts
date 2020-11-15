84af79c0300ba6875df12de361a87a3dadc64ce2
Chubvasz <kareduardo@hotmail.com>
2020-11-15 16:23:46 -0600

    44.Directivas Estricturales *ngFor *ngIf

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,4 +6,12 @@ import { Component } from "@angular/core";
 })
 export class BodyComponent {
 
+  mostrar = true;
+  frase:any = {
+    mensaje: 'Un gran poder, conlleva a una gran responsabilidad',
+    autor: 'Ben Parker',
+    url: 'https://sites.google.com/site/spidermanpp2013/_/rsrc/1468751337798/home/pagina-web-2/benjamin-parker-o-tio-ben/ssssss.jpg?height=300&width=400'
+  };
+  personajes:string[] = ['Spiderman', 'Venom', 'Dr. Octopus']
+
 }
